af9472c3ea1f7655d870870a654d0a5bea5caf73
Makoto Hamanaka <m.hamanaka@psychic-vr-lab.com>
2021-06-06 02:07:39 +0900

    use wasm3 iOS framework / add Mac OS bundle

---
+++ Assets/Wasm3/Plugins/iOS/wasm3.framework/Headers/wasm3_ios_framework.h
@@ -0,0 +1,19 @@
+//
+//  wasm3_ios_framework.h
+//  wasm3_ios_framework
+//
+//  Created by Makoto Hamanaka on 2021/06/06.
+//  Copyright © 2021 wasm3. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for wasm3_ios_framework.
+FOUNDATION_EXPORT double wasm3_ios_frameworkVersionNumber;
+
+//! Project version string for wasm3_ios_framework.
+FOUNDATION_EXPORT const unsigned char wasm3_ios_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <wasm3_ios_framework/PublicHeader.h>
+
+
